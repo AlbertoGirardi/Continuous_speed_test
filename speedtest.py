3e63d800deb0e5f41e2af1479d79e8fbea0c6f40
AlbertoGirardi <alberto.gir747@gmail.com>
2024-06-10 19:04:45 +0200

    color logs

--- speedtest.py
+++ speedtest.py
@@ -8,6 +8,18 @@ from datetime import datetime
 import plotting
 import sys
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 max_retries = 8  # Set the maximum number of retries
 retry_delay = 1  # Initial delay between retries (in seconds)
 
